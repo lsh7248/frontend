e7475ac559ba53757e19219aab0154e78a36e147
LeeSeongHyun <leesh7248@gmail.com>
2023-03-02 23:27:34 +0900

    Error 라우팅 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,29 @@
 import Blog from '@src/pages/Blog';
 import Login from '@src/pages/Login';
+import About from '@src/pages/About';
 import { createBrowserRouter } from 'react-router-dom';
+import Error from '@src/pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Blog />,
-    // errorElement: <ErrorPage />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/blog',
+    element: <Blog />,
+    errorElement: <Error />,
   },
   {
     path: '/login',
     element: <Login />,
-    // errorElement: <ErrorPage />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+    errorElement: <Error />,
   },
 ]);
 
